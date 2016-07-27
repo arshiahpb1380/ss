321ddd26603aeddaf3d7a49bebf8e715390267af
arshiahpb1380 <info.arshiasoftware@gmail.com>
2016-07-27 23:16:11 +0430

    Update user.php

--- user.php
+++ user.php
@@ -2,7 +2,7 @@
 $directory = 'users';
 $users = array_diff(scandir($directory), array('..', '.'));
 
-define('API_KEY','XXXXX');
+define('API_KEY','215431813:AAHi9NgTSebsudgJutfueDP-8cfFbrtm-gY');
 
 function makeHTTPRequest($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY
